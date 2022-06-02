563ab021564a9c952c9d8aaab3dc004273daaafb
barneyricca <barneyricca@gmail.com>
2022-06-02 18:46:32 -0400

    First data comment

---
+++ R/guastello.R
@@ -0,0 +1,11 @@
+#' Sample categorical time series
+#'
+#' A dataset containing a ficticious sequence of categorical data.
+#'
+#' @format  A character vector with 20 elements.
+#'
+#' @source Guastello, S. (2011). Orbital decomposition: Identification of
+#' dynamical patterns in categorical data. in S. Guastello & R. Gregson (Eds.)
+#' Nonlinear dynamical systems analysis for the behavioral sciences using
+#' real data. Boca Raton, FL: CRC Press.
+"guastello"
